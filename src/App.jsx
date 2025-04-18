51137e085a9325df8e4c39996a48a223fd83e74f
ishadiIFSolutions <76896498+HMDITharushika@users.noreply.github.com>
2025-04-18 22:54:49 +0530

    Hero - Completed

--- src/App.jsx
+++ src/App.jsx
@@ -11,28 +11,30 @@ import SuperQuality from "./sections/SuperQuality";
 export default function App() {
   return (
     <main className="relative">
-      <Nav/>
-      <section className="xl:padding-1 wide:padding-r padding-b"><Hero/></section>
-      <section className="padding">
-        <PopularProducts/>
+      <Nav />
+      <section className="xl:padding-l wide:padding-r padding-b">
+        <Hero />
       </section>
       <section className="padding">
-        <SuperQuality/>
+        <PopularProducts />
+      </section>
+      <section className="padding">
+        <SuperQuality />
       </section>
       <section className="padding-x py-10">
-        <Services/>
+        <Services />
       </section>
       <section className="padding">
-        <SpecialOffer/>
+        <SpecialOffer />
       </section>
       <section className="padding bg-pale-blue">
-        <CustomerReviews/>
+        <CustomerReviews />
       </section>
       <section className="padding-x sm:py-32 py-16 w-full">
-        <Subscribe/>
+        <Subscribe />
       </section>
       <section className="padding bg-black padding-x padding-t pb-8">
-        <Footer/>
+        <Footer />
       </section>
     </main>
   );
