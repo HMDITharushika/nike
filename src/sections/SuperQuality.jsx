2c9d6af537c407e5c1763a21a7fc158f4d906b81
ishadiIFSolutions <76896498+HMDITharushika@users.noreply.github.com>
2025-04-19 22:01:29 +0530

    Product Section

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,9 +1,31 @@
+import Button from "../components/Button";
+
 const SuperQuality = () => {
   return (
-    <div>
-      SuperQuality
-    </div>
-  )
-}
+    <section
+      id="about-us"
+      className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container"
+    >
+      <div className="flex flex-1 flex-col">
+        <p className="text-xl font-montserrat text-coral-red">
+          Our Summer Collection
+        </p>
+        <h1 className="mt-18 font-palanquin text-8xl max-sm:text-[72px] max-sm:leadig-[82] font-bold">
+          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
+            The New Arrival
+          </span>
+          <br />
+          <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
+        </h1>
+        <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">
+          Discover stylish Nike arrivals, quality confort, and innovation for
+          your active life.
+        </p>
 
-export default SuperQuality
+        <Button label="View Details" />
+      </div>
+    </section>
+  );
+};
+
+export default SuperQuality;
